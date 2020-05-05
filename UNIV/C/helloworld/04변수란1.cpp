29332cdbf7d0a969bf2d5a4a97d6d86c37ad4746
Shin Soojung <42960479+morriee@users.noreply.github.com>
2020-05-05 20:36:43 +0900

    Add files via upload

---
+++ UNIV/C/helloworld/04변수란1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a; // 선언 : 변수를 만드는 과정
+	a = 3;  // '=' : 대입, 같음이 아님.
+	printf("%d\n", a);
+
+	a = 5;
+	printf("%d\n", a);
+}
---
+++ UNIV/C/helloworld/04변수란2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2; // 초기화 : 선언과 동시에 대입을 할 수 있다.
+	int b = 3;
+
+	printf("%d + %d = %d\n", a, b, a + b);
+
+	// 변수 이름 :  알파벳 대소문자, _(underscore), 숫자(단,첫번째 글자X)
+}
